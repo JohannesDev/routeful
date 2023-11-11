02df11a7588d7bed8d6a1c87d3dc6328c1291c0d
flofriday <flohacksfriday@gmail.com>
2023-11-12 00:53:41 +0100

    Implement simple google api gateway for POI

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 api_key = open("APIKEY.txt").read()
@@ -8,4 +9,6 @@ api_key = open("APIKEY.txt").read()
 @app.route("/search")
 def hello_world():
     query = request.args["q"]
-    return "flotschi"
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.210033,16.363449&radius=12000&keyword={query}&key={api_key}"
+    res = requests.get(url)
+    return res.json()
